9b67a13edb10506af5d6a3cbf2830e9c9f9fe1ca
lekgolo167 <167sonoma182@gmail.com>
2019-07-21 15:30:40 -0600

    progress

--- Main.py
+++ Main.py
@@ -1,23 +1,26 @@
 
 message = 'Hello World'
-
-for char in message:
+maskList = [0, 1, 2, 4, 8, 16, 32, 64]
+bitPosition = [0, 8, 16, 24, 32, 39, 47, 54, 61, 68, 75, 81, 87, 93, 98, 103, 107, 111, 115, 118, 121, 123, 125, 126, 127]
+charCount = 0
+while charCount < len(message):
+    char = message[charCount]
     print(char)
     print("{0:b}".format(ord(char)))
-    mask = 0
-    if (ord(char)&1):
-        mask |= 1 << 1
-    if (ord(char)&2):
-        mask |= 1 << 2
-    if (ord(char)&4):
-        mask |= 1 << 3
-    if (ord(char)&8):
-        mask |= 1 << 4
-    if (ord(char)&16):
-        mask |= 1 << 5
-    if (ord(char)&32):
-        mask |= 1 << 6
-    if (ord(char)&64):
-        mask |= 1 << 7
+    encoded = 0
+    maskCount = 0
 
-    print("{0:b}".format(mask))
+    for i in range(0, len(bitPosition)):
+        if maskCount > 7:
+            maskCount = 0
+            charCount += 1
+            char = message[charCount]
+
+        if ord(char) & maskList[maskCount]:
+            encoded |= 1 << maskCount
+        else:
+            encoded |= 0 << maskCount
+
+        maskCount += 1
+
+    print("{0:b}".format(encoded))
